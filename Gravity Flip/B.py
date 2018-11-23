f43d5e4d7b1d4876302d3d8d385c8ed3a373d36f
Felipe Silva <lfsilva1@uc.cl>
2018-11-22 21:08:03 -0300

    added test and ¿improve memory?

--- Gravity Flip/B.py
+++ Gravity Flip/B.py
@@ -1,5 +1,8 @@
 n = int(input())
-lista = list(map(lambda x: int(x),input().strip().split(" ")))
+lista = list(map(lambda x: int(x), input().strip().split(" ")))
 lista.sort()
-lista = list(map(lambda x: str(x), lista))
-print(" ".join(lista))
+lista2 = list(map(lambda x: str(x), lista))
+del lista
+print(" ".join(lista2))
+del lista2
+del n
